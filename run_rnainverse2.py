a3c9dbeaa3d743c8b4b5ae15ff6072eb2bdd4114
Jan Badura <jan.badura@put.poznan.pl>
2023-11-04 22:18:36 +0100

    ADD: scripts for data2

---
+++ run_rnainverse2.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import subprocess
+
+if len(sys.argv) == 3:
+    range_s = int(sys.argv[1])
+    range_e = int(sys.argv[2])
+else:
+    range_s = 0
+    range_e = 20000
+
+dirs = [('/rna_design/inputs2/rnainverse', '/rna_design/outputs2/rnainverse')]
+
+for indir, outdir in dirs:
+
+    to_do = os.listdir(indir)
+    to_do.sort()
+    to_do = to_do[range_s:range_e]
+    for fn in to_do:
+        tmp = fn.split('.')[0]
+        
+        if os.path.isfile(f'{outdir}/{tmp}.inv.out'):
+            continue
+        
+        outfile = open(f'{outdir}/{tmp}.inv.out', 'w')
+        errfile = open(f'{outdir}/{tmp}.inv.err', 'w')
+
+        infile = open(f'{indir}/{fn}', 'r')
+
+        command = ['time', 'RNAinverse', '-R10']
+        subprocess.run(command, stdin=infile, stdout=outfile, stderr=errfile)
+        
+        infile.close()
+        outfile.close()
+        errfile.close()
+
+        outfile = open(f'{outdir}/{tmp}.inv.out', 'r')
+        outfile2 = open(f'{outdir}/{tmp}.stripped.out', 'w')
+        for l in outfile:
+            seq = l.strip().split()[0]
+            outfile.write(seq + '\n')
+        outfile.close()    
+        outfile2.close()
+
+        outfile = open(f'{outdir}/{tmp}.fold.out', 'w')
+        errfile = open(f'{outdir}/{tmp}.fold.err', 'w')
+
+        infile = open(f'{outdir}/{tmp}.stripped.out', 'r')
+        
+        command = ['time', 'RNAfold']
+        subprocess.run(command, stdin=infile, stdout=outfile, stderr=errfile)
+        
+        infile.close()
+        outfile.close()
+        errfile.close()
---
+++ run_rnasfbinv2.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import subprocess
+
+if len(sys.argv) == 3:
+    range_s = int(sys.argv[1])
+    range_e = int(sys.argv[2])
+else:
+    range_s = 0
+    range_e = 20000
+
+dirs = [('/rna_design/inputs/rnasfbinv', '/rna_design/outputs/rnasfbinv'), 
+        ('/rna_design/inputs/rnasfbinv_extended', '/rna_design/outputs/rnasfbinv_extended')]
+
+for indir, outdir in dirs:
+
+    to_do = os.listdir(indir)
+    to_do.sort()
+    to_do = to_do[range_s:range_e]
+    for fn in to_do:
+        for num in range(10):
+            tmp = fn.split('.')[0]
+            
+            if os.path.isfile(f'{outdir}/{tmp}_{num}.out'):
+                continue
+            
+            outfile = open(f'{outdir}/{tmp}_{num}.out', 'w')
+            errfile = open(f'{outdir}/{tmp}_{num}.err', 'w')
+
+            command = ['time', 'python3', '/RNAsfbinv/RNAfbinvCL.py', '-f', f'{indir}/{fn}']
+            subprocess.run(command, stdout=outfile, stderr=errfile)
+            
+            outfile.close()
+            errfile.close()
+
