0b958045f8fc96584a92a04c07bed21565c2b115
Jan Badura <jan.badura@put.poznan.pl>
2023-11-08 17:53:23 +0100

    FIX: calculate_distance

--- calculate_scores.py
+++ calculate_scores.py
@@ -3,7 +3,7 @@ from minineedle import needle
 import subprocess
 
 def get_needlewunsh_distance(seq1, seq2):
-    if seq1 == '' or seq2 == '':
+    if len(seq1) != len(seq2):
         return 'NA'
 
     a = needle.NeedlemanWunsch(og_seq, al_seq)
@@ -12,7 +12,7 @@ def get_needlewunsh_distance(seq1, seq2):
     return seq_score
 
 def get_rna_distance(str1, str2):
-    if str1 == '' or str2 == '':
+    if len(str1) != len(str2):
         return 'NA'
 
     tmp_f = open('tmp.in', 'w')
@@ -30,58 +30,52 @@ def get_rna_distance(str1, str2):
     line = line.strip().split()
     return line[-1]
 
-f = open('/rna_design/results/results.txt', 'r')
-header = f.readline().strip().split(';')
+def main():
+    f = open('/rna_design/results/results.txt', 'r')
+    header = f.readline().strip().split(';')
 
-if len(sys.argv) != 2:
-    print('No algo name')
-    exit()
-algo = sys.argv[1]
-if 'extended' in algo:
-    print("Don't use extended in algo name")
-    exit()
-algo_i = -1
-for i in range(len(header)):
-    if algo == header[i].split('_')[0]:
-        algo_i = i
-        break
-if algo_i == -1:
-    print('No such algo')
-    exit()
+    if len(sys.argv) != 2:
+        print('No algo name')
+        exit()
+    algo = sys.argv[1]
+    if 'extended' in algo:
+        print("Don't use extended in algo name")
+        exit()
+    algo_i = -1
+    for i in range(len(header)):
+        if algo == header[i].split('_')[0]:
+            algo_i = i
+            break
+    if algo_i == -1:
+        print('No such algo')
+        exit()
+        
+    f_out = open(f'/rna_design/results/{algo}_res.txt', 'w')
+        
+    for l in f:
+        l = l.strip().split(';')
+        
+        ID = l[0]
+        
+        og_seq = l[1]
+        og_str = l[2]
+        al_seq = l[algo_i]
+        al_str = l[algo_i+1]
+        
+        og_seq_ext = l[3]
+        og_str_ext = l[4]
+        al_seq_ext = l[algo_i+2]
+        al_str_ext = l[algo_i+3]
+        
+        score_seq = get_needlewunsh_distance(og_seq, al_seq)
+        score_str = get_rna_distance(og_str, al_str)
+        
+        score_seq_ext = get_needlewunsh_distance(og_seq_ext, al_seq_ext)
+        score_str_ext = get_rna_distance(og_str_ext, al_str_ext)
+        
+        f_out.write(f'{ID};{og_seq};{og_str};{al_seq};{score_seq};{al_str};{score_str};{al_seq_ext};{score_seq_ext};{al_str_ext};{score_str_ext}\n')
+        
+    f.close()
+    f_out.close()
     
-f_out = open(f'/rna_design/results/{algo}_res.txt', 'w')
-    
-for l in f:
-    l = l.strip().split(';')
-    
-    ID = l[0]
-    
-    og_seq = l[1]
-    og_str = l[2]
-    al_seq = l[algo_i]
-    al_str = l[algo_i+1]
-    
-    og_seq_ext = l[3]
-    og_str_ext = l[4]
-    al_seq_ext = l[algo_i+2]
-    al_str_ext = l[algo_i+3]
-    
-    score_seq = get_needlewunsh_distance(og_seq, al_seq)
-    score_str = get_rna_distance(og_str, al_str)
-    
-    score_seq_ext = get_needlewunsh_distance(og_seq_ext, al_seq_ext)
-    score_str_ext = get_rna_distance(og_str_ext, al_str_ext)
-    
-    f_out.write(f'{ID};{og_seq};{og_str};{al_seq};{score_seq};{al_str};{score_str};{al_seq_ext};{score_seq_ext};{al_str_ext};{score_str_ext}\n')
-    
-f.close()
-f_out.close()
-    
-    
-    
-    
-    
-    
-    
-    
-    
+main()    
