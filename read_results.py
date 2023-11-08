6acd1c8fba6d50d7eb6ed46df054e69ccfab4ef6
Jan Badura <jan.badura@put.poznan.pl>
2023-11-08 15:13:11 +0100

    CHG: output result format

--- read_results.py
+++ read_results.py
@@ -16,7 +16,7 @@ results = {}
 for resdir in ['desirna', 'desirna_extended']:
     results[resdir] = {}
     for fn in os.listdir(f'outputs/{resdir}'):
-        if not fn.endswith('.fold.out'): continue
+        if fn.endswith('.err'): continue
         sequence = ''
         structure = ''
         ID = fn.split('.')[0]
@@ -35,7 +35,7 @@ print('Done desirna')
 for resdir in ['rnainverse', 'rnainverse_extended']:
     results[resdir] = {}
     for fn in os.listdir(f'outputs/{resdir}'):
-        if fn.endswith('.err'): continue
+        if not fn.endswith('.fold.out'): continue
         sequence = ''
         structure = ''
         ID = fn.split('.')[0]
@@ -128,14 +128,22 @@ except:
     pass
  
 f = open('data/loops_id.csv', 'r')
-f_o1 = open('results/results.txt', 'w')
-f_o2 = open('results/results_extended.txt', 'w')
-f_mis = open('resu;ts/missing.txt', 'w')
+f_o = open('results/results.txt', 'w')
+f_mis = open('results/missing.txt', 'w')
 
 for k1 in results:
     for k2 in results[k1]:
         if results[k1][k2][0] == '' or results[k1][k2][1]:
             f_mis.write(f'Missing res for {k1} {k2}\n')
+f_mis.close()
+
+algs_order = list(results.keys())
+algs_order.sort()
+
+to_write = ['ID', 'sequence', 'structure', 'sequence_extended', 'structure_extended']
+for algo in algs_order:
+    to_write.append(algo)
+print(';'.join(to_write), file=f_o)
 
 for l in f:
     l = l.strip().split(',')
@@ -145,28 +153,18 @@ for l in f:
     if name == 'Source': continue
     ID = l[-1]
     
-    if ID not in results['desirna']: continue
-    
-    print(ID)
-    
     fragment_sequence = l[5]
     fragment_structure = remove_junk(l[6])
     
     fragment_sequence_ext = l[8]
     fragment_structure_ext = remove_junk(l[9])
     
-    print(f'Original:\t{fragment_sequence}\t{fragment_structure}', file=f_o1)
-    for algo in results:
-        if 'extended' in algo: continue
-        print(f'{algo}:\t{results[algo][ID][0]}\t{results[algo][ID][1]}', file=f_o1)
-    print('------------------------------------------------------------', file=f_o1)
-        
-    print(f'Original:\t{fragment_sequence_ext}\t{fragment_structure_ext}', file=f_o2)
-    for algo in results:
-        if 'extended' not in algo: continue
-        print(f'{algo[:-9]}:\t{results[algo][ID][0]}\t{results[algo][ID][1]}', file=f_o2)
-    print('------------------------------------------------------------', file=f_o2)
+    to_write = [ID, fragment_sequence, fragment_structure, fragment_sequence_ext, fragment_structure_ext]
+    for algo in algs_order:
+        to_write.append(results[algo][ID][0])
+        to_write.append(results[algo][ID][1])
     
+    print(';'.join(to_write), file=f_o)
+
 f.close()
-f_o1.close()
-f_o2.close()
+f_o.close()
